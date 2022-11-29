919c05c88d31b4c0c6bf8f993422d009d8dfa78f
pgodettejr <paul.godette@gmail.com>
2022-11-29 11:06:45 -0500

    Edit rps.html and rps.js
    For the HTML document, added HTML boilerplate and external JSS link
    script in the body. For the JS document, wrote a function that will
    randomly return Rock, Paper or Scissors for our RPS game. Tested the
    function on console in DevTools and it seems to work.

---
+++ rps.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    return String("rock", "paper", "scissors");
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
